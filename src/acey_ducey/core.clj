25421c19dfa1822d77204c88635a7ae2251ed06e
Alejandro Andres <fuzzy.alej@gmail.com>
2014-05-25 13:32:54 +0200

    awesomic game

--- src/acey_ducey/core.clj
+++ src/acey_ducey/core.clj
@@ -4,29 +4,43 @@
 
 (defn get-random
   [n]
-    (sort (take n (distinct (take 5 (repeatedly (partial rand-int 10)))))))
+  (sort (take n (distinct (take 5 (repeatedly (partial rand-int 10)))))))
 
 (defn play
-  [[minimum maximum]]
+  [[minimum maximum] money]
   (let [number (first (get-random 1))]
     (println (str "Ha sido un " number))
     (if (and (> number minimum) (< number maximum))
-      (println "YOU WIN")
-      (println "YOU LOSE"))))
+      (do
+        (println "YOU WIN")
+        (+ money 10))
+      (do
+        (println "YOU LOSE")
+        (- money 10)))))
 
 (defn start
-  []
+  [money]
+  (when (< money 10)
+    (println "Go home you poor bastard, you LOST ALL!!")
+    (System/exit 0))
   (let [numbers (get-random 2)]
     (println (str "The numbers are: " numbers))
+    (println (str "You have " money "$!"))
     (println "Do you want to play? (y/n/q)")
     (let [input (read-line)]
-      (when (= input "q")
-        (System/exit 0))
-      (when (= input "n")
-        (println "ok, let's try again"))
-      (when (= input "y")
-        (play numbers))
-      (start))))
+      (cond
+        (= input "q") (System/exit 0)
+        (= input "n")
+        (do
+          (println "You chicken out! Lost 2$ for it.")
+          (let [money (- money 2)]
+            (start money)))
+        (= input "y") (do
+                        (let [money (play numbers money)]
+                          (start money)))
+        :else (do
+                (println "not a valid option")
+                (start money))))))
 
 (defn -main
   [& args]
@@ -34,4 +48,5 @@
   (println "THIS IS ACEY DUCEY, A GAME FROM THE FUTURE")
   (println "THAT YOU CAN START PLAYING NOW!!!")
   (println)
-  (start))
+  (let [money 100]
+    (start money)))
